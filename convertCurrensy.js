5f7e3f293a0cc83eaa4726e5b2048ab0d1fb72f0
ZhenyaGro <bon4ios@gmail.com>
2022-07-03 17:27:04 +0300

    Added script for converting currencies

--- convertCurrensy.js
+++ convertCurrensy.js
@@ -1,13 +1,41 @@
 'use strict';
+// Free api (1500 requests avaliable) https://app.exchangerate-api.com/dashboard/confirmed
+
+const elCurrencyFrom = document.getElementById('currencyFrom');
+const elCurrencyTo = document.getElementById('currencyTo');
+const elAmountFrom = document.getElementById('amountFrom');
+const elAmountTo = document.getElementById('amountTo');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+function getCurrenciesRates() {
+
+  let currencyFrom = elCurrencyFrom.value;
+  let currencyTo = elCurrencyTo.value;
+
+  fetch(`https://v6.exchangerate-api.com/v6/7fd52bbd6cb5e9fff6ab9110/latest/${currencyFrom}`)
+    .then(response => response.json())
+    .then(data => {
+      let rate = data.conversion_rates[currencyTo];
+      rateEl.innerText = `1 ${currencyFrom} = ${rate} ${currencyTo}`;
+
+      elAmountTo.value = (elAmountFrom.value * rate).toFixed(2);
+    })
+    .catch(error => console.log('Error of getting rates')); // Need to improve
+
+} // Need to add checking of errors (Internet connection or ran out of API requests)
+
+elCurrencyFrom.addEventListener('change', getCurrenciesRates);
+elCurrencyTo.addEventListener('change', getCurrenciesRates);
+elAmountFrom.addEventListener('input', getCurrenciesRates);
+elAmountTo.addEventListener('input', getCurrenciesRates);
+
+swap.addEventListener('click', () => {
+  const temp = elCurrencyFrom.value;
+  elCurrencyFrom.value = elCurrencyTo.value;
+  elCurrencyTo.value = temp;
+  getCurrenciesRates();
+})
 
 getCurrenciesRates();
-
-async function getCurrenciesRates() {
-  let currentRates = await fetch(/* Here will be API */);
-  let curData = await currentRates.json();
-  let currenciesJson = await curData;
-} // Need to add checking of errors
-
-function convertCurrensy(value) {
-  document.getElementById('outputCurrency').innerHTML = value /* * Rate of currency */;
-}
