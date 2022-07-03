609192296b1c835cee15d51820b832dfdb22d6d5
ZhenyaGro <bon4ios@gmail.com>
2022-07-03 20:20:38 +0300

    Now it defines base currency

--- defineLocal.js
+++ defineLocal.js
@@ -16,22 +16,89 @@ if (docLanguage == 'ru' && userLanguage != 'ru') {
 }
 */
 
+elCurrencyTo.value = 'USD';
 switch (userLanguage) {
-  case 'ru':
-    elCurrencyFrom.value = 'RUB';
-    elCurrencyTo.value = 'USD';
-    getCurrenciesRates();
+
+  case 'sq':
+    elCurrencyFrom.value = 'ALL';
     break;
 
-  case 'en':
-    elCurrencyFrom.value = 'USD';
-    elCurrencyTo.value = 'EUR';
-    getCurrenciesRates();
+  case 'ar':
+    elCurrencyFrom.value = 'AED';
+    break;
+
+  case 'ru':
+    elCurrencyFrom.value = 'RUB';
+    break;
+
+  case 'hy':
+    elCurrencyFrom.value = 'AMD';
+    break;
+
+  case 'az':
+    elCurrencyFrom.value = 'AZN';
+    break;
+
+  case 'be':
+    elCurrencyFrom.value = 'BYN';
+    break;
+
+  case 'zh':
+    elCurrencyFrom.value = 'CNY';
+    break;
+
+  case 'cs':
+    elCurrencyFrom.value = 'CZK';
+    break;
+
+  case 'da':
+    elCurrencyFrom.value = 'DKK';
+    break;
+
+  case 'eu':
+  case 'de':
+  case 'et':
+  case 'fr':
+  case 'it':
+  case 'lv':
+  case 'lt':
+  case 'nl':
+  case 'es':
+    elCurrencyFrom.value = 'EUR';
+    break;
+
+  case 'ja':
+    elCurrencyFrom.value = 'JPY';
+    break;
+
+  case 'kk':
+    elCurrencyFrom.value = 'KZT';
+    break;
+
+  case 'pl':
+    elCurrencyFrom.value = 'PLN';
+    break;
+
+  case 'sr':
+    elCurrencyFrom.value = 'RSD';
+    break;
+
+  case 'sv':
+    elCurrencyFrom.value = 'SEK';
+    break;
+
+  case 'tr':
+    elCurrencyFrom.value = 'TRY';
+    break;
+
+  case 'uk':
+    elCurrencyFrom.value = 'UAH';
     break;
 
   default:
     elCurrencyFrom.value = 'USD';
-    elCurrencyTo.value = 'RUB';
-    getCurrenciesRates();
+    elCurrencyTo.value = 'EUR';
     break;
 }
+
+getCurrenciesRates();
