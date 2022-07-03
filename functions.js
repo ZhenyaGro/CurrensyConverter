56a32c8fc02bf0410038b321a335a61500275038
ZhenyaGro <bon4ios@gmail.com>
2022-07-03 22:24:33 +0300

    2-digits after point in rate

--- functions.js
+++ functions.js
@@ -9,7 +9,7 @@ function getCurrenciesRates() {
     .then(response => response.json())
     .then(data => {
       let rate = data.conversion_rates[currencyTo];
-      rateEl.innerText = `1 ${currencyFrom} = ${rate} ${currencyTo}`;
+      rateEl.innerText = `1 ${currencyFrom} = ${rate.toFixed(2)} ${currencyTo}`;
 
       elAmountTo.value = (elAmountFrom.value * rate).toFixed(2);
     })
@@ -36,7 +36,7 @@ function setLanguage() {
 }
 
 function fillCurrencies() {
-  // This is a list of supported by https://app.exchangerate-api.com/ currencies
+  // The lis of supported currencies got here: https://www.exchangerate-api.com/docs/supported-currencies
   let currencies = ["USD", "EUR", "RUB", "AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTN", "BWP", "BYN", "BZD", "CAD", "CDF", "CHF", "CLP", "CNY", "COP", "CRC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "FJD", "FKP", "FOK", "GBP", "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF", "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KID", "KMF", "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRU", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLL", "SOS", "SRD", "SSP", "STN", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TVD", "TWD", "TZS", "UAH", "UGX", "UYU", "UZS", "VES", "VND", "VUV", "WST", "XAF", "XCD", "XDR", "XOF", "XPF", "YER", "ZAR", "ZMW", "ZWL"];
 
   for (let currency of currencies) {
