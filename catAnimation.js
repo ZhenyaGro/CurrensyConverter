7eb5a2072efbca643193ccd0c138ebaf9da40965
ZhenyaGro <bon4ios@gmail.com>
2022-07-05 21:06:21 +0300

    Added the cat animation!

---
+++ catAnimation.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Cat animation
+let looper;
+let degrees = 1;
+let clockwise = true;
+function rotateAnimation(el, speed) {
+  let elem = document.getElementById(el);
+  elem.style.WebkitTransform = "rotate(" + degrees + "deg)";
+  looper = setTimeout('rotateAnimation(\'' + el + '\',' + speed + ')', speed);
+  if (degrees >= 25 || degrees <= 0) {
+    clockwise = !clockwise;
+  }
+
+  if (clockwise) {
+    degrees++;
+  } else if (!clockwise) {
+    degrees--;
+  }
+}
