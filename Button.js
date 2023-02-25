7fd80844d7d8366ed1925e4da8d41f68c1392440
ZhenyaGro <bon4ios@gmail.com>
2023-02-25 23:36:24 +0300

    Number filling when buttons click

--- Button.js
+++ Button.js
@@ -1,12 +1,17 @@
 class Button {
   _element;
   _value;
+  _elAmount = document.getElementById('amount-from');
 
   constructor(element, value) {
     this._element = element;
     this._value = value;
 
     this._element.addEventListener('click', () => console.log(value));
+    this._element.addEventListener('click', () => {
+      if (this._value == -1)
+        this._elAmount.value = this._elAmount.value.slice(0, -1);
+      else this._elAmount.value += this._value;
+    });
   }
-
 }
