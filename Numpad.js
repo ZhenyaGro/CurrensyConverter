e957e300538d8c6e1b2b142309876f6abbd58d1f
ZhenyaGro <bon4ios@gmail.com>
2023-02-25 20:50:32 +0300

    Universal value getting

--- Numpad.js
+++ Numpad.js
@@ -1,13 +1,14 @@
 class Numpad {
-  _button1;
-  _button2;
+  _buttons;
 
   constructor() {
-    let testButton1 = document.getElementById('button-value-1');
-    let testButton2 = document.getElementById('button-value-2');
+    this._buttons = document
+      .getElementById('numpad')
+      .querySelectorAll('button');
 
-    this._button1 = new Button(testButton1, 1);
-    this._button2 = new Button(testButton2, 2);
+    this._buttons.forEach(element => {
+      new Button(element, element.value);
+    });
   }
 
 }
