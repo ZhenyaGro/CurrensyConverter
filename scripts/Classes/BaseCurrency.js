d6700a41dfe47902770f82f99557c40e6789158f
ZhenyaGro <bon4ios@gmail.com>
2023-05-10 22:19:51 +0300

    Structure continue

---
+++ scripts/Classes/BaseCurrency.js
@@ -0,0 +1,8 @@
+class BaseCurrency extends Currency {
+  #userLanguage = window.navigator.language.substr(0, 2);
+
+  constructor(/* Get browser's language */) {
+    // Set base currency
+    // Set currency to convert
+  }
+}
--- scripts/Classes/Currency.js
+++ scripts/Classes/Currency.js
@@ -9,12 +9,3 @@ class Currency {
     this.#country = country;
   }
 }
-
-class BaseCurrency extends Currency {
-  #userLanguage = window.navigator.language.substr(0, 2);
-
-  constructor(/* Get browser's language */) {
-    // Set base currency
-    // Set currency to convert
-  }
-}
